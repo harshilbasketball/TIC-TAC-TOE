41af07650da10f027823928a53b11b0b9e0c5777
harshilbasketball <harshilbasketball0531@gmail.com>
2026-04-20 14:58:53 +0530

    UC4: Slot to row-column conversion

--- UC4.java
+++ UC4.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class UC4 {
 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int slot = 7;
+        int slot = getUserSlot();
         System.out.println("Row: " + getRowFromSlot(slot));
         System.out.println("Column: " + getColFromSlot(slot));
+    }
+
+    static int getUserSlot() {
+        System.out.print("Enter slot (1-9): ");
+        return sc.nextInt();
     }
 
     static int getRowFromSlot(int slot) {
