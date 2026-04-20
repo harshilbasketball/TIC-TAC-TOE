13d7234b55efa4b0f20f47bc49815e236949e2fe
harshilbasketball <harshilbasketball0531@gmail.com>
2026-04-20 14:54:06 +0530

    UC4: CONVERT SLOT NUMBER TO BOARD INDEX

--- UC3.java
+++ UC3.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
 
-<<<<<<< HEAD
 public class UC3 {
-=======
-public class TicTacToe {
->>>>>>> bca71313b210cee39eff36549f28a50ff68b4a6a
 
     static Scanner sc = new Scanner(System.in);
 
@@ -18,8 +14,4 @@ public class TicTacToe {
         int slot = sc.nextInt();
         return slot;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> bca71313b210cee39eff36549f28a50ff68b4a6a
